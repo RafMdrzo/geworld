a70e96955399c64c1fc5d53cfcb551a765c39d4d
RafMdrzo <rafael_maderazo@dlsu.edu.ph>
2021-06-29 14:41:47 +0800

    deploy

--- public/js/home.js
+++ public/js/home.js
@@ -2,15 +2,13 @@ $( document ).ready(function()
 {
     
     
-    $(".hidden").hide().fadeIn(1500);
+   
 
-    $("#acts").click(function()
-    {
-        window.location.href = "/activities";
-    });
+    $("#dis-blog").prop("disabled", true);
+
     var typed = new Typed('#pls', {
-        strings:["Hi! Welcome to GEWORLD-G1's creative output.", "This is in partial fulfilment for our course."],
-        backSpeed: 55,
+        strings:["Hi! Welcome to GEWORLD-G1's website.", "We'll be posting all of our group's output here. This is in partial fulfilment for our course.", "Head on over to creative outputs and we hope you enjoy!"],
+        backSpeed: 65,
         typeSpeed: 55
     });
 
