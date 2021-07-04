9d68a30fbe9e5f1ce4f6c97575d397e67433f6f2
RafMdrzo <rafael_maderazo@dlsu.edu.ph>
2021-07-04 12:42:00 +0800

    section headers 2 created

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,27 @@
 $( document ).ready(function()
 {
 
-    
     function typeIt(text) {
         var bounds = new Typed('#bounds', {
-            strings:["Territories, Boundaries, and Rights."],
+            strings:["On Territories, Boundaries, and Rights."],
             backSpeed: 65,
             typeSpeed: 55
         });
     }
+
     $("#create1").click(function()
     {
         window.location.href = "/create1";
     });
+    try{
+        var typed = new Typed('#fincred1', {
+            strings:["Creative Output 1."],
+            backSpeed: 65,
+            typeSpeed: 55
+        });
+    } catch {
+        console.log("");
+    }
     try{
         var typed = new Typed('#pls', {
             strings:["Hi! Welcome to GEWORLD-G1's website.", "We'll be posting all of our group's output here. This is in partial fulfilment for our course.", "Head on over to creative outputs and we hope you enjoy!"],
